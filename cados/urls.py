679911b1368cb31e841616c2306c55cbebcf6481
Salvin Lopes <salvinlopes20@gmail.com>
2022-10-26 16:54:06 +0530

    static files configured

--- cados/urls.py
+++ cados/urls.py
@@ -2,8 +2,16 @@
 from django.contrib import admin
 from django.urls import path,include
 
+# static files /logos configuring
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('api/', include('base.api.urls')),
 ]
+
+# static files /logos configuring
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
